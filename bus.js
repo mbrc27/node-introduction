4ab45972edc78bd45d4a487d6d1fa2472916956b
Marcin Brach <marcin.brc@gmail.com>
2017-07-20 18:03:44 +0200

    5th

--- bus.js
+++ bus.js
@@ -1,18 +1,39 @@
 const Promise = require("bluebird");
 const _ = require("lodash");
+const fetch = require("isomorphic-fetch");
 const readFile = Promise.promisify(require("fs").readFile);
 
-const location = { lon: 21.0518, lat: 52.1296 };
+const BUS_API_URL = "https://api.um.warszawa.pl/api/action/busestrams_get";
+const BUS_API_KEY = "d3cd9ec8-1920-43c7-a557-8a0e8443cc93";
+const LOCATION_API_URL = "http://ipinfo.io/json";
 
-const getBuses = cb => {
+const getLocation = () => {
+  return fetch(LOCATION_API_URL).then(response => {
+    if (response.status !== 200) throw new Error("Bad request");
+    return response.json();
+  });
+};
+
+const getBuses = () => {
+  return fetch(
+    `${BUS_API_URL}/?resource_id=%20f2e5503e-%20927d-4ad3-9500-4ab9e55deb59&apikey=${BUS_API_KEY}&type=1&line=503`
+  ).then(response => {
+    if (response.status !== 200) throw new Error("Bad request");
+    return response.json();
+  });
+};
+
+const getBusesByLocation = () => {
   return new Promise((resolve, reject) => {
-    readFile("./503.json", "utf8")
-      .then(data => {
-        const buses = JSON.parse(data).result;
+    Promise.all([getLocation(), getBuses()])
+      .then(responses => {
+        const [{ loc }, busData] = responses;
+        const location = { lat: +loc.split(",")[0], lon: +loc.split(",")[1] };
+        const buses = busData.result;
         const busesInRange = _.chain(buses)
           .filter(
             bus =>
-              bus.Lat - location.lat <= 0.001 && bus.Lon - location.lon <= 0.001
+              bus.Lat - location.lat <= 0.1 && bus.Lon - location.lon <= 0.1
           )
           .intersectionBy("Lines")
           .value();
@@ -23,5 +44,5 @@ const getBuses = cb => {
   });
 };
 
-module.exports = { getBuses };
+module.exports = { getBusesByLocation };
 //exports.getBuses;
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const { getBuses } = require("./bus");
+const http = require("http");
+const { getBusesByLocation } = require("./bus");
 
-getBuses().then(buses => console.log(buses)).catch(err => console.error(err));
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  getBusesByLocation()
+    .then(buses => {
+      res.statusCode = 200;
+      res.end(JSON.stringify(buses, null, 2));
+    })
+    .catch(err => {
+      res.statusCode = 400;
+      res.end(err.message);
+    });
+});
+
+server.listen(3000);
+
+console.log("Server listening on port 3000");
